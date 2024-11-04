a8990a3d035eb0c21f545c1a7744f8b25e7a6ee6
petnacode <peternnadi1999@gmail.com>
2024-11-04 10:22:34 +0100

    remove unnecessary import file from the signup page

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,10 +1,10 @@
-import SignUpScreen from '@/src/screens/onboarding/SignUpScreen'
+
 import React from 'react'
 
 function page() {
   return (
     <div className='bg-black w-full p-10'>
-      <SignUpScreen />
+     
     </div>
   )
 }
