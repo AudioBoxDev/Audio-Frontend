e319c7120217fee264d937fb7fdefc46bb9ae725
petnacode <peternnadi1999@gmail.com>
2024-12-17 08:59:20 +0100

    work on the start listening logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import AudioBoxFeatures from "@/components/AudioBoxFeatures";
 import AudioCardItem from "@/components/AudioCardItem";
 import Footer from "@/components/Footer";
@@ -6,54 +6,76 @@ import Hero from "@/components/Hero";
 import Navbar2 from "@/components/Navbar2";
 import Image from "next/image";
 import headset from "/public/images/img.png";
-import Link from "next/link";
-import Artist from "@/components/Artist";
+import { uploadProfileDetails } from "@/hooks/uploadProfileDetails";
+import { useAccount } from "wagmi";
+import Cookies from "js-cookie";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-  <>
-  {/* <WIPBanner/> */}
-    <Navbar2/>
-      <Hero/>
-    <AudioBoxFeatures/>
-    <AudioCardItem/>
-   <Artist/>
-    <div className="bg-gradient-to-r from-[#991E7035] to-[#333A5670] text-white py-6">
-      <div className="grid w-11/12 m-auto items-center md:grid-cols-5 grid-cols-12 gap-6">
-      <div className="md:col-span-2 col-span-12 md:text-left text-center md:pl-16 space-y-5">
-        <h1 className="text-3xl md:text-5xl font-bold leading-tight">
-          Stream,Support, and Own it!
-        </h1>
-        <p className="text-lg md:text-xl text-white">
-          Join a community where your support makes a real difference, and every play, tip, or purchase helps artists thrive.
-        </p>
-        <div>
+	const { artistProfileDetails } = uploadProfileDetails();
+	const { isConnected } = useAccount();
+	const token = Cookies.get("audioblocks_jwt");
+	const router = useRouter();
 
-        <Link href="/login" className="bg-gradient-to-r from-[#B1198E] to-[#B81A3F] text-white text-sm px-4 py-2 rounded-full">Get Started</Link>
-        </div>
-      </div>
-      <div className="md:col-span-3 col-span-12">
-        <Image
-          className="w-full md:w-96 mx-auto"
-          src={headset}
-          alt="Headphone"
-        />
-      </div>
-      </div>
-    </div>
-    <div className="bg-[#471439] text-white py-4">
-      <div className="container mx-auto flex items-center justify-center space-x-2">
-        <span className="text-sm font-bold">Powered by</span>
-        <img
-          src="/images/lisk.png" 
-          alt="Lisk Logo"
-          className="h-12"
-        />
-        <span className="text-sm font-bold">Lisk</span>
-      </div>
-    </div>
-    <Footer/>
-  </>
-    
-  );
+	const getStarted = () => {
+		if (!token) {
+			if (!isConnected) {
+				toast.error("Please connect your wallet");
+			} else {
+				toast.error("Please sign the Authentication message");
+			}
+		} else {
+			const redirectPath = artistProfileDetails
+				? "/dashboard"
+				: "/dashboard/profile";
+			router.push(redirectPath);
+		}
+	};
+
+	return (
+		<>
+			<Navbar2 />
+			<Hero />
+			<AudioBoxFeatures />
+			<AudioCardItem />
+
+			<div className="bg-gradient-to-r from-[#991E7035] to-[#333A5670] text-white py-6">
+				<div className="grid w-11/12 m-auto items-center md:grid-cols-5 grid-cols-12 gap-6">
+					<div className="md:col-span-2 col-span-12 md:text-left text-center md:pl-16 space-y-5">
+						<h1 className="text-3xl md:text-5xl font-bold leading-tight">
+							Stream,Support, and Own it!
+						</h1>
+						<p className="text-lg md:text-xl text-white">
+							Join a community where your support makes a real difference, and
+							every play, tip, or purchase helps artists thrive.
+						</p>
+						<div>
+							<button
+								onClick={getStarted}
+								className="bg-gradient-to-r from-[#B1198E] to-[#B81A3F] text-white text-sm px-4 py-2 rounded-full"
+							>
+								Get Started
+							</button>
+						</div>
+					</div>
+					<div className="md:col-span-3 col-span-12">
+						<Image
+							className="w-full md:w-96 mx-auto"
+							src={headset}
+							alt="Headphone"
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="bg-[#471439] text-white py-4">
+				<div className="container mx-auto flex items-center justify-center space-x-2">
+					<span className="text-sm font-bold">Powered by</span>
+					<img src="/images/lisk.png" alt="Lisk Logo" className="h-12" />
+					<span className="text-sm font-bold">Lisk</span>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
