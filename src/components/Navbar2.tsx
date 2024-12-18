e380ff077766cddd7bb8d5a8de4e35b9f2e58ea1
Wilfred007 <adzerwilfred007@hmail.com>
2024-12-18 12:56:54 +0100

    artist

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -64,7 +64,8 @@ const Navbar2 = () => {
 			<nav className="items-center font-roboto w-11/12 m-auto text-white py-7 flex justify-between">
 				<div className="space-x-10 flex items-center">
 					<Link href="/" className="flex space-x-3 items-center">
-						<div className="bg-pink-500 rounded-full md:h-10 h-7 w-7 md:w-10"></div>
+						{/* <div className="bg-pink-500 rounded-full md:h-10 h-7 w-7 md:w-10"></div> */}
+						<img src="/images/logo.jpeg" height={40} width={40} alt="logo" className="rounded-full" />
 						<h1 className="md:text-2xl text-lg font-semibold text-pink-400">
 							AudioBlocks
 						</h1>
