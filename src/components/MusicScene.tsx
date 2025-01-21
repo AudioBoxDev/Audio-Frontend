e1d8179a7313ac29fc54646d55b00101beb3c856
Petnacode <60398083+peternnadi1999@users.noreply.github.com>
2025-01-21 01:23:36 +0100

    Update MusicScene.tsx

--- src/components/MusicScene.tsx
+++ src/components/MusicScene.tsx
@@ -7,7 +7,6 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { fetchArtist } from "@/hooks/fetchArtist";
 
-import { useRouter } from "next/navigation";
 import { fetchArtist } from "@/hooks/fetchArtist";
 
 
