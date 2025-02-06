7f55fb522bf8ff6fcb677184eddb052f753db704
petnacode <peternnadi1999@gmail.com>
2025-02-06 21:21:34 +0100

    adjusted the top wip element

--- src/components/Wim.tsx
+++ src/components/Wim.tsx
@@ -7,7 +7,7 @@ export const WIPBanner = () => {
 
 	return (
 		<>
-			<div className="bg-yellow-500 text-black top-0 sticky z-100 text-center py-2">
+			<div className="bg-yellow-500 text-black top-0 sticky z-50 text-center py-2">
 				<p>
 					{" "}
 					🚧 We are currently under development. Some features may not be fully
